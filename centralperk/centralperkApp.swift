4ce89bcca69a4de8c57384a75d37a655f0840e75
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-09-10 14:47:56 -0400

    Initial Commit

---
+++ centralperk/centralperkApp.swift
@@ -0,0 +1,17 @@
+//
+//  centralperkApp.swift
+//  centralperk
+//
+//  Created by Tanmai Kalisipudi on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct centralperkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
